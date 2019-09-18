abd88ea7c28319a00e042351872a87d5e3038a52
Aaron Muliawan <47096278+AaronMuliawan@users.noreply.github.com>
2019-09-19 01:21:05 +0700

    Curiculum Vitae
    Cv android

---
+++ CuriculumVitae/app/src/main/java/com/example/curiculumvitae/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.curiculumvitae;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn1 = findViewById(R.id.tombol1);
+        ImageButton btn2 = findViewById(R.id.tombol2);
+        ImageButton btn3 = findViewById(R.id.tombol3);
+        ImageButton btn4 = findViewById(R.id.tombol4);
+        ImageButton btn5 = findViewById(R.id.tombol5);
+        ImageButton btn6 = findViewById(R.id.tombol6);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Projek.class);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Pendidikan.class);
+                startActivity(intent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Keahlian.class);
+                startActivity(intent);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Kontak.class);
+                startActivity(intent);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Sertfikat.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
